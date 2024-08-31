25f2324a26b0a618a0506a7b3db74b790d64ff8f
wellandrade <leto.l@hotmail.com>
2024-08-31 02:08:13 -0300

    feat: add plans section on web site

---
+++ js/script.js
@@ -0,0 +1,148 @@
+function getPlanosHTML() {
+  return `
+          <div class="flex flex-wrap justify-center gap-4">
+    <!-- Card 200 MEGA -->
+    <div class="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden border border-gray-200">
+        <div class="bg-blue-700 text-white text-center py-4">
+            <h2 class="text-2xl font-bold">200 MEGA</h2>
+            <p class="text-sm text-yellow-500">FJ CONNECT</p>
+        </div>
+        <div class="text-center py-6">
+            <p class="text-lg">R$</p>
+            <p class="text-4xl font-bold">74,90</p>
+            <p class="text-sm">/mensal</p>
+        </div>
+        <div class="px-6 py-4 text-gray-700">
+            <ul class="space-y-2">
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Fibra ótica</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Upload simétrico</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Suporte presencial</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Equipamento em comodato</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Wi-Fi 5G Incluso</li>
+            </ul>
+        </div>
+        <div class="px-6 mt-20 contratar">
+            <a href="https://api.whatsapp.com/send?phone=+558141029031&text=Quero%20assinar%20o%20plano%20200MEGA." class="block bg-yellow-600 text-white text-center py-2 rounded-full hover:bg-yellow-500">Contratar</a>
+        </div>
+    </div>
+
+    <!-- Card 400 MEGA -->
+    <div class="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden border border-gray-200">
+        <div class="bg-blue-700 text-white text-center py-4">
+            <h2 class="text-2xl font-bold">400 MEGA</h2>
+            <p class="text-sm text-yellow-500">FJ SUPER</p>
+        </div>
+        <div class="text-center py-6">
+            <p class="text-lg">R$</p>
+            <p class="text-4xl font-bold">84,90</p>
+            <p class="text-sm">/mensal</p>
+        </div>
+        <div class="px-6 py-4 text-gray-700">
+            <ul class="space-y-2">
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Fibra ótica</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Upload simétrico</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Suporte presencial</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Equipamento em comodato</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Wi-Fi 5G Incluso</li>
+            </ul>
+        </div>
+        <div class="px-6 mt-20 contratar">
+            <a href="https://api.whatsapp.com/send?phone=+558141029031&text=Quero%20assinar%20o%20plano%20400MEGA." class="block bg-yellow-600 text-white text-center py-2 rounded-full hover:bg-yellow-500">Contratar</a>
+        </div>
+    </div>
+
+    <!-- Card 500 MEGA -->
+    <div class="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden border border-gray-200">
+        <div class="bg-blue-700 text-white text-center py-4">
+            <h2 class="text-2xl font-bold">500 MEGA</h2>
+            <p class="text-sm text-yellow-500">FJ HIPER</p>
+        </div>
+        <div class="text-center py-6">
+            <p class="text-lg">R$</p>
+            <p class="text-4xl font-bold">94,90</p>
+            <p class="text-sm">/mensal</p>
+             </div>
+        <div class="px-6 py-4 text-gray-700">
+            <ul class="space-y-2">
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Fibra ótica</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Upload simétrico</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Suporte presencial</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Equipamento em comodato</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Wi-Fi 5G Incluso</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Streaming</li>
+            </ul>
+        </div>
+        <div class="px-6 mt-12 contratar">
+            <a href="https://api.whatsapp.com/send?phone=+558141029031&text=Quero%20assinar%20o%20plano%20500MEGA." class="block bg-yellow-600 text-white text-center py-2 rounded-full hover:bg-yellow-500">Contratar</a>
+        </div>
+    </div>
+
+    <!-- Card 650 MEGA -->
+    <div class="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden border border-gray-200">
+        <div class="bg-blue-700 text-white text-center py-4">
+            <h2 class="text-2xl font-bold">650 MEGA</h2>
+            <p class="text-sm text-yellow-500">FJ MEGA</p>
+        </div>
+        <div class="text-center py-6">
+            <p class="text-lg">R$</p>
+            <p class="text-4xl font-bold">109,90</p>
+            <p class="text-sm">/mensal</p>
+        </div>
+        <div class="px-6 py-4 text-gray-700">
+            <ul class="space-y-2">
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Fibra ótica</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Upload simétrico</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Suporte presencial</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Equipamento em comodato</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Wi-Fi 5G Incluso</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Streaming</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Telefonia Fixa Inclusa</li>
+            </ul>
+        </div>
+        <div class="px-6 py-4 contratar">
+            <a href="https://api.whatsapp.com/send?phone=+558141029031&text=Quero%20assinar%20o%20plano%20650MEGA." class="block bg-yellow-600 text-white text-center py-2 rounded-full hover:bg-yellow-500">Contratar</a>
+        </div>
+    </div>
+
+    <!-- Card 1GB -->
+    <div class="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden border border-gray-200">
+        <div class="bg-blue-700 text-white text-center py-4">
+            <h2 class="text-2xl font-bold">1 GB</h2>
+            <p class="text-sm text-yellow-500">FJ ULTRA</p>
+        </div>
+        <div class="text-center py-6">
+            <p class="text-lg">R$</p>
+            <p class="text-4xl font-bold">179,90</p>
+            <p class="text-sm">/mensal</p>
+        </div>
+        <div class="px-6 py-4 text-gray-700">
+            <ul class="space-y-2">
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Fibra ótica</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Upload simétrico</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Suporte presencial</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Equipamento em comodato</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Wi-Fi 6G Incluso</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Streaming</li>
+                <li class="flex items-center"><svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>Telefonia Fixa Inclusa</li>
+            </ul>
+        </div>
+        <div class="contratar px-6 py-4">
+            <a href="https://api.whatsapp.com/send?phone=+558141029031&text=Quero%20assinar%20o%20plano%201GB." class="block bg-yellow-600 text-white text-center py-2 rounded-full hover:bg-yellow-500">Contratar</a>
+        </div>
+    </div>
+</div>
+
+
+      `;
+}
+
+function renderPlanos() {
+  document.getElementById("planos-container").innerHTML = getPlanosHTML();
+}
+
+document.addEventListener("DOMContentLoaded", renderPlanos);
+
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  var menu = document.getElementById("menu");
+  menu.classList.toggle("hidden");
+});
