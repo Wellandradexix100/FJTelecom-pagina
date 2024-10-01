a7a753d0ae0a0a8844f42e9eab7a4e116cd8d514
wellandrade <leto.l@hotmail.com>
2024-09-30 21:07:31 -0300

    fix: responsives values

--- js/script.js
+++ js/script.js
@@ -148,3 +148,24 @@ document.getElementById("menu-toggle").addEventListener("click", function () {
 });
 
 let index = 0;
+
+let currentSlide = 0;
+const slides = document.querySelectorAll("#carousel > div");
+
+function showSlide(index) {
+  const totalSlides = slides.length;
+  currentSlide = (index + totalSlides) % totalSlides;
+  const offset = -currentSlide * 100;
+  document.getElementById(
+    "carousel"
+  ).style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+  showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+  showSlide(currentSlide - 1);
+}
+setInterval(nextSlide, 10000);
